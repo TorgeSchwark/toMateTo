2aca75ecbe8b0247b35012cbf846a11e9ce40e97
torgeschwark <torge.schwark@outlook.de>
2026-09-05 16:23:13 +0200

    Perft positions 1-6 Work untill depth of 7 move gen integrity done

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@
 #include "toMateTo_engine/testing/debugging.h"
 #include "testing/stockfish_perft.h"
 
+// pos 4: r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1
+// pos 5: rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  
+// pos 6: r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10
+// pos 7: 
+
 
 int main() {
     chess_board gui_board;
@@ -55,7 +60,7 @@ int main() {
     //test_position();
     
  
-   find_perft_error("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
+   find_perft_error("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
     
     
     // chess_board board;
